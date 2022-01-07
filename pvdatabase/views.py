2a5a7ec6af6020adfe2aab99a3dcf293f42dbb54
Vinay Konuru <vkonuru@princeton.edu>
2022-01-06 21:27:17 -0500

    hooking up search button

--- pvdatabase/views.py
+++ pvdatabase/views.py
@@ -6,7 +6,8 @@ import csv
 
 def home(request):
     return render(request,'home.html')
-
+def search(request):
+    return render(request, 'search.html')
 # def about(request):
 #     return render(request,'about.html')
 #
