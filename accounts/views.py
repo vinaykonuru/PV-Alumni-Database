15b0c5987f3f62cce8f507c7d3be69c8b75b366d
Vinay Konuru <vkonuru@princeton.edu>
2022-01-08 18:51:56 -0500

    test

--- accounts/views.py
+++ accounts/views.py
@@ -19,13 +19,13 @@ def signup(request):
         if request.POST['password1']==request.POST['password2']:
             try:
                 user=User.objects.get(username=request.POST['username'])
-                return render(request, 'accounts/signup.html', {'error':'Username has already been taken'})
+                return render(request, 'signup.html', {'error':'Username has already been taken'})
             except User.DoesNotExist:
                 user=User.objects.create_user(request.POST['username'],password=request.POST['password1'])
                 auth.login(request,user)
                 return redirect('home')
         else:
-            return render(request,'accounts/signup.html',{'error':'Passwords must match'})
+            return render(request,'signup.html',{'error':'Passwords must match'})
     else:
         return render(request, 'signup.html')
 
