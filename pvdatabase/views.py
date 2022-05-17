791573c843b717e49178099c92213349f6eaf11e
Vinay Konuru <vinaykonuru@gmail.com>
2022-05-17 18:20:08 -0400

    fix name/id in search.html

--- pvdatabase/views.py
+++ pvdatabase/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect,get_obj0ect_or_404
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
