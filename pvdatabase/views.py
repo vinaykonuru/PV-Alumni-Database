9b0961e8f495013723d422ea5fbba5871cc4ea98
Vinay Konuru <vinaykonuru@gmail.com>
2022-05-17 18:42:30 -0400

    import model

--- pvdatabase/views.py
+++ pvdatabase/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from alumniprof.models import AlumniProf
 
 import csv
 
