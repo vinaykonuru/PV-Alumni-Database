309c9130b4a0ff222ff3e098be9002b0815a431f
Vinay Konuru <vinaykonuru@gmail.com>
2022-05-17 23:38:21 -0400

    editing names

--- pvdatabase/settings.py
+++ pvdatabase/settings.py
@@ -130,19 +130,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
-
-# STATICFILES_DIRS=[
-#     os.path.join(BASE_DIR, 'static')
-# ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+django_heroku.settings(locals())
+
 try:
-  from .local_settings import *
+    from .local_settings import *
 except ImportError:
-  pass
+    pass
