7381433157f173607f28422706e6b1391b1e1843
Vinay Konuru <vinaykonuru@gmail.com>
2022-05-22 18:25:22 -0400

    test

--- pvdatabase/settings.py
+++ pvdatabase/settings.py
@@ -148,3 +148,11 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'pvalumnidb@gmail.com'
+EMAIL_HOST_PASSWORD = 'pv2022'
